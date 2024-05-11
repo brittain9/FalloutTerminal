6fd13b2d48dc3985a3ebd3ff266751b0be37ec30
alex b <alexbrittain9@gmail.com>
2024-05-11 16:12:37 -0500

    fixed hex numbers

--- Frames/HexFrameLogic.cs
+++ Frames/HexFrameLogic.cs
@@ -11,7 +11,7 @@ namespace fot
         {
             "able", "acid", "aged", "also", "area", "army",
             "away", "baby", "back", "ball", "band", "bank",
-            "base", "bath", "bear", "beat", "bed",  "beer",
+            "base", "bath", "bear", "beat",  "beer",
             "bell", "belt", "best", "bill", "bird", "blue",
             "boat", "body", "bond", "bone", "book", "boom",
             "born", "boss", "both", "bowl", "bulk", "burn",
@@ -19,13 +19,12 @@ namespace fot
             "calm", "came", "camp", "card", "care", "case"
         };
 
-        private string hexNumberStart;
+        private int nextHexNumber;
 
         public HexFrameLogic()
         {
-            hexNumberStart = null;
-
             Random random = new Random();
+            nextHexNumber = random.Next(0x1000, 0x10000);
             int randomIndex = random.Next(0, fourLetterWords.Count);
             GameStatistics.CorrectWord = fourLetterWords[randomIndex];
     }
@@ -40,7 +39,7 @@ namespace fot
         private void CreateLine(int Y) 
         { 
             StringBuilder sb = new StringBuilder();
-            string hexNumber = GetNextHexNumber(hexNumberStart);
+            string hexNumber = GetHexNumber();
             var hexNumLabel = new Label(0, Y, hexNumber); // 0 because its always first
             FalloutTerminal.HexFrame.Add(hexNumLabel);
 
@@ -58,23 +57,11 @@ namespace fot
             ButtonClicked?.Invoke(this, chosenWord);
         }
         
-        private string GetNextHexNumber(string existingHex = null)
+        private string GetHexNumber()
         {
-            // If no existing hex number, generate a new random 4-digit hex
-            if (string.IsNullOrEmpty(existingHex))
-            {
-                hexNumberStart = existingHex;
-                Random random = new Random();
-                int randomNumber = random.Next(0x1000, 0x10000); // Random number between 0x1000 and 0xFFFF
-                return randomNumber.ToString("X4"); // Convert to 4-digit hex string
-            }
-            else
-            {
-                // Parse the existing hex number and increment it
-                int existingNumber = int.Parse(existingHex, System.Globalization.NumberStyles.HexNumber);
-                int nextNumber = (existingNumber + 1) & 0xFFFF; // Increment and wrap around to 0x0000 if exceeds 0xFFFF
-                return nextNumber.ToString("X4"); // Convert to 4-digit hex string
-            }
+                int nextNumber = (nextHexNumber + 1) & 0xFFFF; // Increment and wrap around to 0x0000 if exceeds 0xFFFF
+                nextHexNumber++;
+                return "0x" + nextNumber.ToString("X4"); // Convert to 4-digit hex string
         }
     }
 }
